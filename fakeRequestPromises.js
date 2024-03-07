44801d6a66ddaf4373bf29f5e23e581273095f6b
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-03-07 17:11:52 +0530

    Nested Promises

---
+++ fakeRequestPromises.js
@@ -0,0 +1,45 @@
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * 4500) + 500;
+        setTimeout(() => {
+            if(delay > 4000) {
+                failure( "Error: Request took too long!" );
+            } else{
+                success(`Here is your fake Data! from ${url}`);
+            }   
+        },delay)
+    })
+};
+
+//isnt neccessay to put in variable
+// const request = fakeRequestPromise('books.com/good/coffee');
+//  request.then(() => {
+//     console.log("It worked");
+//  }).catch(() => {
+//     console.error("It didn't work");
+//  })
+
+fakeRequestPromise('books.com/good/coffee/page1');
+    request.then(() => {
+        console.log("It worked for page 1");
+        fakeRequestPromise('books.com/good/coffee/page2');
+        request.then(() => {
+            console.log("It worked for page 2");
+            fakeRequestPromise('books.com/good/coffee/page1');
+            request.then(() => {
+                console.log("It worked for page 3");
+                fakeRequestPromise('books.com/good/coffee/page1');
+                request.then(() => {
+                    console.log("It worked for page 4");
+                }).catch(() => {
+                    console.error("It didn't work for page 4");
+                })
+            }).catch(() => {
+                console.error("It didn't work for page 3");
+            })
+        }).catch(() => {
+            console.error("It didn't work for page 2");
+        })
+    }).catch(() => {
+        console.error("It didn't work page 1");
+    })
