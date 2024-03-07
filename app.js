2b2d351bd005e5ba6dbbdd082d5e6373dc81dbff
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-03-07 12:40:24 +0530

    Nested delayed color change

--- app.js
+++ app.js
@@ -5,18 +5,20 @@
 // setTimeout( () => { document.body.style.backgroundColor = 'brown';
 // }, 2000);
 
-setTimeout( () => { 
-    document.body.style.backgroundColor = 'olive';
-    setTimeout( () => { 
-        document.body.style.backgroundColor = 'pink';
-        setTimeout( () => { 
-            document.body.style.backgroundColor = 'brown';
-            setTimeout( () => { 
-                document.body.style.backgroundColor = 'coral';
-                setTimeout( () => { 
-                    document.body.style.backgroundColor = 'burlywood';
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
+// setTimeout( () => { 
+//     document.body.style.backgroundColor = 'olive';
+//     setTimeout( () => { 
+//         document.body.style.backgroundColor = 'pink';
+//         setTimeout( () => { 
+//             document.body.style.backgroundColor = 'brown';
+//             setTimeout( () => { 
+//                 document.body.style.backgroundColor = 'coral';
+//                 setTimeout( () => { 
+//                     document.body.style.backgroundColor = 'burlywood';
+//                     }, 1000);
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+
+
