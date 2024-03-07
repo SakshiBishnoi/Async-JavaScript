9337982bba447f58859d853c63a398e0debb63fe
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-03-07 15:18:09 +0530

    Super nested promises

--- fakeRequestCB.js
+++ fakeRequestCB.js
@@ -1,17 +1,30 @@
-fakeRequestCB = ((url,success,failure) => {
+fakeRequestCB = (url,success,failure) => {
     const delay = Math.floor(Math.random() * 4500) + 500;
-    setTimeout = ( () => {
-        if(delay > 4000)
+    setTimeout(() => {
+        if(delay > 4000) {
             failure( "Error: Request took too long!" );
-        else
+        } else{
             success(`Here is your fake Data! from ${url}`);
+        }   
     },delay)
-});
+};
 
-
-fakeRequestCB('books.com',
+fakeRequestCB('books.com/page1',
     function(response){ 
-        console.log('It worked' ,response )
+        console.log('It worked\n' ,response )
+        fakeRequestCB('books.com/page2',
+        function(response){
+            console.log('\nSecond request also worked!\n' , response);
+            fakeRequestCB('books.com/page3',
+                function(response){
+                console.log('\nYepppiieeee Third request worked!\n' , response);
+                },
+                function(error){
+                console.log("AHHHHH Third one failed...\n" + error);
+            })
+        },function(error){
+            console.log("But the second one failed...\n" + error);
+        })
     }, function (error){
-        console.log( 'Failed to get data: ', error);
-    });
+        console.log( 'Failed to get data:\n',  error);
+});
