3a7384d0c70674fa575c3dfff849f3112efc3f01
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-03-07 14:04:11 +0530

    renamed

---
+++ fakeRequestCB.js
@@ -0,0 +1,9 @@
+fakeRequestCB = ((url,success,failure) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout = ( () => {
+        if(delay > 4000)
+            failure( "Error: Request took too long!" );
+        else
+            success(`Here is your fake Data! from ${url}`);
+    },delay)
+});
