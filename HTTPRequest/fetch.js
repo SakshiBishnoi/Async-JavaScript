6961d2f2f3fd7a9c6c2df5767e73ffb719083706
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-01 22:28:15 +0530

    Chained but independent

--- HTTPRequest/fetch.js
+++ HTTPRequest/fetch.js
@@ -24,6 +24,9 @@ const star = async() => {
     const res = await fetch("https://swapi.dev/api/people/1/");
     const data = await res.json();
     console.log(data);
+    const res2 = await fetch("https://swapi.dev/api/people/1/");
+    const data2 = await res2.json();
+    console.log(data2);
 };
 
 star();
