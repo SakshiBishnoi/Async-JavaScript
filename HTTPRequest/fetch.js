db521131fcb203bb3d4e24ba8d690c4a9c24e987
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-01 22:18:28 +0530

    Chained Request in fetch api

--- HTTPRequest/fetch.js
+++ HTTPRequest/fetch.js
@@ -5,6 +5,15 @@ fetch("https://swapi.dev/api/people/1/")
     })
     .then((data) => {
         console.log(data);
+        return fetch("https://swapi.dev/api/people/1/")
+    })
+    .then((res) => {
+        console.log("Resoleved",res);
+        return res.json(); 
+    })
+    .then((data) => {
+        console.log("Second Request resolved");
+        
     })
        
     .catch((e) => {
