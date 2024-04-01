fa0d2bf5d67c5e2ec039df14c57d1352552f7f92
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-01 22:27:36 +0530

    Independent request

--- HTTPRequest/fetch.js
+++ HTTPRequest/fetch.js
@@ -1,22 +1,29 @@
-fetch("https://swapi.dev/api/people/1/")
-    .then((res) => {
-        console.log("Resoleved",res);
-        return res.json(); 
-    })
-    .then((data) => {
-        console.log(data);
-        return fetch("https://swapi.dev/api/people/1/")
-    })
-    .then((res) => {
-        console.log("Resoleved",res);
-        return res.json(); 
-    })
-    .then((data) => {
-        console.log("Second Request resolved");
+// fetch("https://swapi.dev/api/people/1/")
+//     .then((res) => {
+//         console.log("Resoleved",res);
+//         return res.json(); 
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         return fetch("https://swapi.dev/api/people/1/")
+//     })
+//     .then((res) => {
+//         console.log("Resoleved",res);
+//         return res.json(); 
+//     })
+//     .then((data) => {
+//         console.log("Second Request resolved");
         
-    })
+//     })
        
-    .catch((e) => {
-        console.log("Rejected", e);
-    });
+//     .catch((e) => {
+//         console.log("Rejected", e);
+//     });
 
+const star = async() => {
+    const res = await fetch("https://swapi.dev/api/people/1/");
+    const data = await res.json();
+    console.log(data);
+};
+
+star();
