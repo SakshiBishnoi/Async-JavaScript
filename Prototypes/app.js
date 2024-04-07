fae947e3608576a2d7fec68c2db76f6f680fd11b
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-07 17:54:40 +0530

    More on classes and methods with rgb colors

--- Prototypes/app.js
+++ Prototypes/app.js
@@ -47,5 +47,28 @@ class Color{
     greet(){
         return `Hello I am ${this.name}`;
     }
+    innerRGB(){
+        const {r,g,b} = this;
+        return  `${r},${g},${b}`;
+    };
+    // rgb(){
+    //     const{r,g,b} = this;
+    //     return `rgb(${r},${g},${b})`;
+    // }
+    rgb(){
+        return `rgb(this.innerRGB())`;
+    }
+    // rgba(){
+    //     const{r,g,b} = this;
+    //     return `rgb(${r},${g},${b},0.7)`;
+    // }
+    rgb(a=0.5){
+        return `rgba(this.innerRGB(),${a})`;
+    }
+    hex(){
+        const{r,g,b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
 }
-const myColor = new Color(19,78,95,'Sea');
+const Blue = new Color(19,78,95,'Sea');
+const Cyan =  new Color(230,126,34,"Cyan");
