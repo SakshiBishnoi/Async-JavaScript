e8b7da4873fca9db02b86acc23303a228d68b141
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-07 15:50:51 +0530

    prototypes

---
+++ Prototypes/app.js
@@ -0,0 +1,19 @@
+String.prototype.yell = function() {
+    return `OMG ${this.toUpperCase()} !!!!!!!!!!!!`;
+}
+
+Array.prototype.pop = function() {
+    return `Hey its an array prototype!`;
+}
+
+
+
+// const navColor = 'rgb[230,126,34]';
+// const logoColor = 'rgb[19, 78, 95]';
+
+// document.querySelectorAll('h1')[0].style.backgroundColor = navColor.rgb();
+// document.querySelectorAll('h2')[0].style.backgroundColor = navColor.rgba(0.9);
+// document.querySelectorAll('h3')[0].style.backgroundColor = navColor.opposite();
+// document.querySelectorAll('h4')[0].style.backgroundColor = navColor.fullSaturation();
+
+
