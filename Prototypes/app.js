e15262e7f736ef7d78394a1f05b63f225bc397dd
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-04-07 17:29:46 +0530

    Consuctor function and colors

--- Prototypes/app.js
+++ Prototypes/app.js
@@ -17,3 +17,22 @@ Array.prototype.pop = function() {
 // document.querySelectorAll('h4')[0].style.backgroundColor = navColor.fullSaturation();
 
 
+function Color(r,g,b){
+    this.r = r;
+    this.g = g;
+    this.b = b;
+};
+
+Color.prototype.rgb = function(){
+   return `rgb(${this.r},${this.g},${this.b})`; //const{r,g,b}=this;
+};
+Color.prototype.rgba= function (a = 0.7) {
+    return `rgb(${this.r},${this.g},${this.b}  , ${a})` ;
+};
+Color.prototype.hex = function () {
+    const{r,g,b} = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+const color1 = new Color(19,78,95);
+const color2  = new Color(230,126,34);
